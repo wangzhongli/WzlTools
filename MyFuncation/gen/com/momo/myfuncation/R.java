c6b9ec2dca98d48e2c8b4cafbe956f29f273fe25
wangzhongli <wangzhongli@gmail.com>
2015-03-30 10:00:43 +0800

    增加

--- MyFuncation/gen/com/momo/myfuncation/R.java
+++ MyFuncation/gen/com/momo/myfuncation/R.java
@@ -16,7 +16,39 @@ theme attribute (in the form
 "<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
 containing a value of this type.
          */
+        public static final int animAlphaStart=0x7f010003;
+        /** <p>Must be an integer value, such as "<code>100</code>".
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+         */
+        public static final int animDuration=0x7f010002;
+        /** <p>Must be a reference to another resource, in the form "<code>@[+][<i>package</i>:]<i>type</i>:<i>name</i></code>"
+or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>".
+         */
+        public static final int collapseDrawable=0x7f010005;
+        /** <p>Must be a reference to another resource, in the form "<code>@[+][<i>package</i>:]<i>type</i>:<i>name</i></code>"
+or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>".
+         */
+        public static final int expandDrawable=0x7f010004;
+        /** <p>Must be a floating point value, such as "<code>1.2</code>".
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+         */
         public static final int heightScale=0x7f010000;
+        /** <p>Must be an integer value, such as "<code>100</code>".
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+         */
+        public static final int maxCollapsedLines=0x7f010001;
     }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
@@ -37,18 +69,24 @@ containing a value of this type.
         public static final int checkswitch_mask=0x7f020004;
         public static final int clear_normal=0x7f020005;
         public static final int clear_pressed=0x7f020006;
-        public static final int ic_launcher=0x7f020007;
-        public static final int selector_bg_delete=0x7f020008;
+        public static final int ic_collapse_small_holo_light=0x7f020007;
+        public static final int ic_expand_small_holo_light=0x7f020008;
+        public static final int ic_launcher=0x7f020009;
+        public static final int selector_bg_delete=0x7f02000a;
     }
     public static final class id {
-        public static final int action_settings=0x7f080003;
-        public static final int item_iv=0x7f080001;
-        public static final int item_tv=0x7f080002;
+        public static final int action_settings=0x7f080006;
+        public static final int expand_collapse=0x7f080003;
+        public static final int expand_text_view=0x7f080001;
+        public static final int expandable_text=0x7f080002;
+        public static final int item_iv=0x7f080004;
+        public static final int item_tv=0x7f080005;
         public static final int list=0x7f080000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
-        public static final int item=0x7f030001;
+        public static final int expandable_text=0x7f030001;
+        public static final int item=0x7f030002;
     }
     public static final class menu {
         public static final int main=0x7f070000;
@@ -87,6 +125,90 @@ containing a value of this type.
         public static final int AppTheme=0x7f060001;
     }
     public static final class styleable {
+        /** Attributes that can be used with a ExpandableTextView.
+           <p>Includes the following attributes:</p>
+           <table>
+           <colgroup align="left" />
+           <colgroup align="left" />
+           <tr><th>Attribute</th><th>Description</th></tr>
+           <tr><td><code>{@link #ExpandableTextView_animAlphaStart com.momo.myfuncation:animAlphaStart}</code></td><td></td></tr>
+           <tr><td><code>{@link #ExpandableTextView_animDuration com.momo.myfuncation:animDuration}</code></td><td></td></tr>
+           <tr><td><code>{@link #ExpandableTextView_collapseDrawable com.momo.myfuncation:collapseDrawable}</code></td><td></td></tr>
+           <tr><td><code>{@link #ExpandableTextView_expandDrawable com.momo.myfuncation:expandDrawable}</code></td><td></td></tr>
+           <tr><td><code>{@link #ExpandableTextView_maxCollapsedLines com.momo.myfuncation:maxCollapsedLines}</code></td><td></td></tr>
+           </table>
+           @see #ExpandableTextView_animAlphaStart
+           @see #ExpandableTextView_animDuration
+           @see #ExpandableTextView_collapseDrawable
+           @see #ExpandableTextView_expandDrawable
+           @see #ExpandableTextView_maxCollapsedLines
+         */
+        public static final int[] ExpandableTextView = {
+            0x7f010001, 0x7f010002, 0x7f010003, 0x7f010004,
+            0x7f010005
+        };
+        /**
+          <p>This symbol is the offset where the {@link com.momo.myfuncation.R.attr#animAlphaStart}
+          attribute's value can be found in the {@link #ExpandableTextView} array.
+
+
+          <p>Must be a floating point value, such as "<code>1.2</code>".
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+          @attr name com.momo.myfuncation:animAlphaStart
+        */
+        public static final int ExpandableTextView_animAlphaStart = 2;
+        /**
+          <p>This symbol is the offset where the {@link com.momo.myfuncation.R.attr#animDuration}
+          attribute's value can be found in the {@link #ExpandableTextView} array.
+
+
+          <p>Must be an integer value, such as "<code>100</code>".
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+          @attr name com.momo.myfuncation:animDuration
+        */
+        public static final int ExpandableTextView_animDuration = 1;
+        /**
+          <p>This symbol is the offset where the {@link com.momo.myfuncation.R.attr#collapseDrawable}
+          attribute's value can be found in the {@link #ExpandableTextView} array.
+
+
+          <p>Must be a reference to another resource, in the form "<code>@[+][<i>package</i>:]<i>type</i>:<i>name</i></code>"
+or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>".
+          @attr name com.momo.myfuncation:collapseDrawable
+        */
+        public static final int ExpandableTextView_collapseDrawable = 4;
+        /**
+          <p>This symbol is the offset where the {@link com.momo.myfuncation.R.attr#expandDrawable}
+          attribute's value can be found in the {@link #ExpandableTextView} array.
+
+
+          <p>Must be a reference to another resource, in the form "<code>@[+][<i>package</i>:]<i>type</i>:<i>name</i></code>"
+or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>".
+          @attr name com.momo.myfuncation:expandDrawable
+        */
+        public static final int ExpandableTextView_expandDrawable = 3;
+        /**
+          <p>This symbol is the offset where the {@link com.momo.myfuncation.R.attr#maxCollapsedLines}
+          attribute's value can be found in the {@link #ExpandableTextView} array.
+
+
+          <p>Must be an integer value, such as "<code>100</code>".
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+          @attr name com.momo.myfuncation:maxCollapsedLines
+        */
+        public static final int ExpandableTextView_maxCollapsedLines = 0;
         /** Attributes that can be used with a ScaleFrameLayout.
            <p>Includes the following attributes:</p>
            <table>
